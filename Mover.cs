af52fcfaf73e4d3373da0772b79219dd99f22ce9
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-03 15:18:38 +0530

    Add files via upload

--- Mover.cs
+++ Mover.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.4f;
-    [SerializeField] float zValue = 0f;
+    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
         
     }
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
 }
