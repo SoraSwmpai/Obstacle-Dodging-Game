0868f4018e8aa47e92c9a5489c28807c11c5e814
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-03 13:21:21 +0530

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.4f;
+    [SerializeField] float zValue = 0f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
