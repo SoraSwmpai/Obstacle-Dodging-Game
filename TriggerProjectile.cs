dd1dab0d457a67c4af8dddc5876f009f8e1fc1f4
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-06 16:57:13 +0530

    Update TriggerProjectile.cs

--- TriggerProjectile.cs
+++ TriggerProjectile.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class TriggerProjectile : MonoBehaviour
 {
-    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject projectile0;
+    [SerializeField] private GameObject projectile1;
+    [SerializeField] private GameObject projectile2;
+    [SerializeField] private GameObject projectile3;
+    [SerializeField] private GameObject projectile4;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            projectile.SetActive(true);
+            projectile0.SetActive(true);
+            projectile1.SetActive(true);
+            projectile2.SetActive(true);
+            projectile3.SetActive(true);
+            projectile4.SetActive(true);
         }
     }
 }
