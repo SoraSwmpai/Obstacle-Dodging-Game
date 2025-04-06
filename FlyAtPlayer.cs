b84dddf2f5efc13bb07d1fae70396bdba4ca4b8a
Sora <99982155+SoraSwmpai@users.noreply.github.com>
2025-04-06 17:40:16 +0530

    Update FlyAtPlayer.cs

--- FlyAtPlayer.cs
+++ FlyAtPlayer.cs
@@ -1,28 +1,22 @@
-using System;
 using UnityEngine;
-
 public class FlyAtPlayer : MonoBehaviour
 {
     Vector3 playerPosition;
     [SerializeField] Transform player; 
     [SerializeField] float projectileSpeed = 0.1f;
-
     void Awake()
     {
         gameObject.SetActive(false);
     }
-
     void Start()
     {
         playerPosition = player.transform.position;
     }
     void Update()
     {
-        
         DestroyWhenReached();
         MoveToPlayer();
     }
-
     void DestroyWhenReached()
     {
         if (transform.position == playerPosition)
@@ -30,7 +24,6 @@ public class FlyAtPlayer : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
     void MoveToPlayer()
     {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * projectileSpeed);
